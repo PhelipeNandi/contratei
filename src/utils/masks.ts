80fe56435d544064f5fa152daafc66b9c12505b1
Phelipe Nandi <phelipenandi@gmail.com>
2022-08-11 15:55:33 -0300

    Ajustado mask do CPF

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -13,5 +13,6 @@ export const normalizeCPF = (value: string | undefined) => {
     return value.replace(/[\D]/g, '')
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1-$2');
+        .replace(/(\d{3})(\d)/, '$1-$2')
+        .replace(/(-\d{2})(\d+?)/, '$1');
 }
