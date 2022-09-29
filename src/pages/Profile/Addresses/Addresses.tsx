2d0914738989ec9d9e18cc72f11aacc783833dfb
Phelipe Nandi <phelipenandi@gmail.com>
2022-09-29 18:51:27 -0300

    Ajustado bug do alert ao cadastar novo endereço

--- src/pages/Profile/Addresses/Addresses.tsx
+++ src/pages/Profile/Addresses/Addresses.tsx
@@ -30,73 +30,75 @@ export function Addresses() {
         <VStack flex={1} bg="background">
             <Header title="Endereços" />
 
-            <Collapse mt={2} isOpen={addressContext.isModalOpen}>
-                <Alert
-                    status="success"
-                    header="Cadastro realizado com sucesso!"
-                    onPress={() => addressContext.setIsModalOpen(false)}
-                />
-            </Collapse>
+            <ScrollView>
+                <Collapse mt={2} isOpen={addressContext.isModalOpen}>
+                    <Alert
+                        status="success"
+                        header="Cadastro realizado com sucesso!"
+                        onPress={() => addressContext.setIsModalOpen(false)}
+                    />
+                </Collapse>
 
-            <VStack mt={5} flex={1}>
-                {
-                    isLoading &&
-                    <Loading />
-                }
+                <VStack mt={5} flex={1}>
+                    {
+                        isLoading &&
+                        <Loading />
+                    }
 
-                {
-                    isError &&
-                    <Center flex={1}>
-                        <Warning color={colors.red[600]} size={32} />
-                        <Text mt={4} textAlign="center" color="gray.300" fontFamily="body" fontSize="sm">
-                            Aconteceu um erro ao buscar seus enderços
-                        </Text>
-                    </Center>
-                }
+                    {
+                        isError &&
+                        <Center flex={1}>
+                            <Warning color={colors.red[600]} size={32} />
+                            <Text mt={4} textAlign="center" color="gray.300" fontFamily="body" fontSize="sm">
+                                Aconteceu um erro ao buscar seus enderços
+                            </Text>
+                        </Center>
+                    }
 
-                {
-                    !isLoading && !isError && (data === undefined || data.length === 0) &&
-                    <Center flex={1}>
-                        <AddressBook color={colors.primary[700]} size={32} />
-                        <Text mt={4} textAlign="center" color="gray.300" fontFamily="body" fontSize="sm">
-                            Você ainda não possui nenhum {"\n"}
-                            endereço cadastrado
-                        </Text>
-                    </Center>
-                }
+                    {
+                        !isLoading && !isError && (data === undefined || data.length === 0) &&
+                        <Center flex={1}>
+                            <AddressBook color={colors.primary[700]} size={32} />
+                            <Text mt={4} textAlign="center" color="gray.300" fontFamily="body" fontSize="sm">
+                                Você ainda não possui nenhum {"\n"}
+                                endereço cadastrado
+                            </Text>
+                        </Center>
+                    }
 
-                {
-                    isSuccess &&
-                    data.map((userAddress) => {
-                        return (
-                            <AddressCard
-                                px={2}
-                                mb={3}
-                                data={userAddress}
-                                key={userAddress.id.toString()}
-                                onPress={() => {
-                                    addressContext.setAddress(userAddress);
-                                    addressContext.setIsEditing(true);
-                                    navigation.navigate("addNewAddress");
-                                }}
-                            />
-                        )
-                    })
-                }
+                    {
+                        isSuccess &&
+                        data.map((userAddress) => {
+                            return (
+                                <AddressCard
+                                    px={2}
+                                    mb={3}
+                                    data={userAddress}
+                                    key={userAddress.id.toString()}
+                                    onPress={() => {
+                                        addressContext.setAddress(userAddress);
+                                        addressContext.setIsEditing(true);
+                                        navigation.navigate("addNewAddress");
+                                    }}
+                                />
+                            )
+                        })
+                    }
 
-                <Button
-                    mx={5}
-                    mt={2}
-                    mb={5}
-                    title="Adicionar"
-                    variant="primary"
-                    onPress={() => {
-                        addressContext.setAddress(null);
-                        addressContext.setIsEditing(false);
-                        navigation.navigate("addNewAddress");
-                    }}
-                />
-            </VStack>
+                    <Button
+                        mx={5}
+                        mt={2}
+                        mb={5}
+                        title="Adicionar"
+                        variant="primary"
+                        onPress={() => {
+                            addressContext.setAddress(null);
+                            addressContext.setIsEditing(false);
+                            navigation.navigate("addNewAddress");
+                        }}
+                    />
+                </VStack>
+            </ScrollView>
         </VStack>
     );
 }
