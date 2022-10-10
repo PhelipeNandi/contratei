79b578f3e195093938dbfd0e9ac8000f593bbe74
Phelipe Nandi <phelipenandi@gmail.com>
2022-10-10 18:32:11 -0300

    Disabilitado query de photosProviderPersonalInformation para consumidores

--- src/pages/Profile/PersonalInformation.tsx
+++ src/pages/Profile/PersonalInformation.tsx
@@ -112,6 +112,7 @@ export function PersonalInformation() {
         isLoading: isLoadingPhotosProvider,
         isError: isErrorPhotosProvider
     } = useQuery("photosProviderPersonalInformation", () => searchPhotosProvider(user.id), {
+        enabled: !isConsumer,
         onSuccess: (data) => {
             setPhotosProvider(data);
         }
